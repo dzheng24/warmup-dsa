d914e024b9fe55ba9b1b25b85058a696b9a17a0b
David Zheng <zhengdav@msu.edu>
2020-03-23 17:47:06 -0700

    added queue class

---
+++ stacks-queues/queues.js
@@ -0,0 +1,63 @@
+// 3/23/20 Monday 
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+    let dequeued = this.first;
+    if (this.length === 1) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return dequeued;
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.enqueue('Samir');
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue);
+console.log(myQueue.dequeue());
+console.log(myQueue);
+//Joy
+//Matt
+//Pavel
+//Samir 
+
